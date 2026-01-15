0f27b6fa1fbb433ce0271ebec6313f10f184b97e
Noel <Noel>
2026-01-15 10:37:30 +0100

    css added

--- config/config-example.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-// Ezt file-t töltsd ki a megfelelő adatokkal majd nevezd át config.php
-return [
-    'db_host' => 'localhost',
-    'db_name' => 'adatbazis',
-    'db_user' => 'felhasznalo',
-    'db_pass' => 'TITKOS_JELSZO'
-];
--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Todo</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+<h1>To do lista adatbázis</h1>
 <input type="text" id="title" placeholder="Új todo">
 <button id="add">Hozzáadás</button>
 
